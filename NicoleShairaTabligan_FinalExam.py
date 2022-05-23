1f4a66d85218259a979073ce24af7770df84779b
NicoleShairaTabligan <98940439+NicoleShairaTabligan@users.noreply.github.com>
2022-05-23 11:45:32 +0800

    Add files via upload

---
+++ NicoleShairaTabligan_FinalExam.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+window = Tk()
+window.title("Find The Smallest Number")
+window.geometry("400x300+20+10")
+
+def findLeast():
+    Numbers = []
+    Numbers.append(eval(firstNumber.get()))
+    Numbers.append(eval(secondNumber.get()))
+    Numbers.append(eval(thirdNumber.get()))
+    smallestNumber.set(min(Numbers))
+
+Label_1 = Label(window, text = "Finds The Smallest Number")
+Label_1.grid(row=0, column=1, columnspan=2,sticky=EW)
+
+Label_2 = Label(window,text = "Enter the first number:")
+Label_2.grid(row=1, column = 0,sticky=W)
+firstNumber = StringVar()
+input_2 = Entry(window,bd=3,textvariable=firstNumber)
+input_2.grid(row=1, column = 1)
+
+Label_3 = Label(window,text = "Enter the second number:")
+Label_3.grid(row=2, column=0)
+secondNumber=StringVar()
+input_3 = Entry(window,bd=3,textvariable=secondNumber)
+input_3.grid(row=2,column=1)
+
+Label_4 = Label(window,text="Enter the third number:")
+Label_4.grid(row=3,column =0, sticky=W)
+thirdNumber = StringVar()
+input_4 = Entry(window,bd=3,textvariable=thirdNumber)
+input_4.grid(row=3, column=1)
+
+buttonSmallest = Button(window,text = "Find The Smallest Number",command=findLeast)
+buttonSmallest.grid(row=4, column = 1)
+
+Label_5 = Label(window,text="The Smallest Number: ")
+Label_5.grid(row=5,column=0,sticky=W)
+smallestNumber = StringVar()
+input_5 = Entry(window,bd=3,state="readonly",textvariable=smallestNumber)
+input_5.grid(row=5,column=1)
+
+mainloop()
